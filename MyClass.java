f925d903425e649bb0706bd740edd29a504c26a1
flavinhatec <137125266+flavinhatec@users.noreply.github.com>
2023-06-20 20:34:10 -0300

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner leitor =new Scanner{System}
+     System .out.print{perfil}
+          String nome= leitor.netx {}
+          System.out.pristla{depositar retiradas histórico}
+
+      System.out.println("Sum of x+y = " + z);
+    }
+}
